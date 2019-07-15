f869c380578d5bf92b7a3fe72de5a8e7cae19b26
Guillaume Mary <guiom.mary@gmail.com>
2019-07-15 23:11:22 +0200

    First step : prepare Stalactite environment

---
+++ src/main/java/org/springframework/samples/petclinic/system/StalactiteConfiguration.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.petclinic.system;
+
+import org.gama.sql.spring.PlatformTransactionManagerConnectionProvider;
+import org.gama.stalactite.persistence.engine.PersistenceContext;
+import org.gama.stalactite.persistence.sql.HSQLDBDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * @author Guillaume Mary
+ */
+@Configuration
+public class StalactiteConfiguration {
+    
+    @Bean
+    public PersistenceContext buildPersistenceContext(PlatformTransactionManager transactionManager) {
+        return new PersistenceContext(new PlatformTransactionManagerConnectionProvider(transactionManager), new HSQLDBDialect());
+    }
+}
